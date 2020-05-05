d0af3fbd36446ac4a29e6f8f803d8caa27c2053b
Nariyuki Saito <toshincompos@gmail.com>
2020-05-05 20:18:00 +0900

    Added backup cronjob

--- docker_builds/docker_build.sh
+++ docker_builds/docker_build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 ES_IMAGE_NAME=elasticsearch-crowi
+BACKUP_IMAGE_NAME=backup-crowi
 REPOSITORY=nakaakist
 docker image build -t ${ES_IMAGE_NAME} ./elasticsearch
 docker image tag ${ES_IMAGE_NAME} ${REPOSITORY}/${ES_IMAGE_NAME}
 docker image push ${REPOSITORY}/${ES_IMAGE_NAME}
+
+docker image build -t ${BACKUP_IMAGE_NAME} ./backup
+docker image tag ${BACKUP_IMAGE_NAME} ${REPOSITORY}/${BACKUP_IMAGE_NAME}
+docker image push ${REPOSITORY}/${BACKUP_IMAGE_NAME}
